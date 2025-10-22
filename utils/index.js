92782f177d7f1e31c954126b31a516eca293254b
Hossein <amraee2004@gmail.com>
2025-10-22 15:52:00 +0330

    initial commit

---
+++ utils/index.js
@@ -0,0 +1,12 @@
+const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt');
+const createTokenUser = require('./createTokenUser');
+const checkPermission = require('./checkPermissions');
+
+
+module.exports = {
+    createJWT,
+    isTokenValid,
+    attachCookiesToResponse,
+    createTokenUser,
+    checkPermission
+};
